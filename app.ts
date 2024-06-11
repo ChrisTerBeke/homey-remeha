50fc72a0333732afa02faf4547c209c7e514d11b
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-06-11 14:11:53 +0200

    Ignore overriding private this.homeyLog

--- app.ts
+++ app.ts
@@ -2,10 +2,12 @@ import sourceMapSupport from 'source-map-support'
 sourceMapSupport.install()
 
 import { App } from 'homey'
-import { Log } from 'homey-log'
+
+const { Log } = require('homey-log')
 
 class RemehaApp extends App {
   async onInit() {
+    // @ts-ignore TS2339
     this.homeyLog = new Log({ homey: this.homey })
     this.log('RemehaApp is running...')
   }
