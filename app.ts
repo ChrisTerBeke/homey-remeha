4880264152385f1a41efb20d2d7d1adc12c1d177
Chris ter Beke <chris@MBP-van-Chris.localdomain>
2024-08-10 20:00:39 +0200

    Remove source maps, remote logging

--- app.ts
+++ app.ts
@@ -1,14 +1,7 @@
-import sourceMapSupport from 'source-map-support'
-sourceMapSupport.install()
-
 import { App } from 'homey'
-
-const { Log } = require('homey-log')
 
 class RemehaApp extends App {
   async onInit() {
-    // @ts-ignore TS2339
-    this.homeyLog = new Log({ homey: this.homey })
     this.log('RemehaApp is running...')
   }
 }
